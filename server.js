1659c7433e648427a7bbb0387ac96c19d1127d02
arman4hossain <95018506+arman4hossain@users.noreply.github.com>
2025-04-02 12:37:41 +0600

    vercel-testing

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ const app = express();
 
 
 app.use(cors());
-app.use(cors({ origin: 'http://localhost:5173' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -30,3 +29,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+
+module.exports = app;
